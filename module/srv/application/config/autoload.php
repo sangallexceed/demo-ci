f5cfb99b2c2eccbde1c536dface1d43b81793793
kimduan <duannk@allexceed.vn>
2017-05-04 16:45:32 +0700

    update source function get ip address

--- module/srv/application/config/autoload.php
+++ module/srv/application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array( 'form', 'url', 'array', 'cookie', 'validation', 'file');
+$autoload['helper'] = array( 'form', 'url', 'array', 'cookie', 'validation', 'file', 'my_common');
 
 /*
 | -------------------------------------------------------------------
